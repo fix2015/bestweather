2c783ca095317e402eb27f62cbaff88d669d7688
semianchuk <fix20152@gmail.com>
2018-03-17 16:17:17 +0200

    add port for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var public = __dirname + "/build/";
-
+var port = process.env.PORT || 8080;
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(public + "index.html"));
